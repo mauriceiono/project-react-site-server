c4103f02e11633ac68aae1fcf80f15a208168a37
mauriciono <mthooks@email.sc.edu>
2024-12-05 18:10:06 -0500

    testing add

--- server.js
+++ server.js
@@ -174,67 +174,6 @@ app.get('/api/images', async (req, res) => {
     res.json(images);
 });
 
-// New endpoint to get hardcoded characters from CharacterList.js
-app.get('/api/CharacterList', async (req, res) => {
-    const characterList = [
-        {
-            id: "mario",
-            name: "Mario",
-            description: "The beloved plumber who saves the Mushroom Kingdom.",
-            image: "images/mario.jpg"
-        },
-        {
-            id: "sonic",
-            name: "Sonic the Hedgehog",
-            description: "The fastest hedgehog who loves adventure.",
-            image: "images/sonic.jpg"
-        },
-        {
-            id: "link",
-            name: "Link",
-            description: "The courageous hero on a quest to save Princess Zelda.",
-            image: "images/link.jpg"
-        },
-        {
-            id: "bowser",
-            name: "Bowser",
-            description: "The main antagonist of the Mario series.",
-            image: "images/bowser.jpg"
-        },
-        {
-            id: "eggman",
-            name: "Dr. Eggman",
-            description: "Sonic’s arch-nemesis with a love for robotics.",
-            image: "images/eggman.jpg"
-        },
-        {
-            id: "ganondorf",
-            name: "Ganondorf",
-            description: "Link's arch nemesis.",
-            image: "images/ganondorf.jpg"
-        },
-        {
-            id: "luigi",
-            name: "Luigi",
-            description: "Mario’s younger brother and loyal companion.",
-            image: "images/luigi.jpg"
-        },
-        {
-            id: "tails",
-            name: "Miles \"Tails\" Prower",
-            description: "Sonic’s two-tailed fox friend and tech genius.",
-            image: "images/tails.jpg"
-        },
-        {
-            id: "fi",
-            name: "Fi",
-            description: "The spirit of the Master Sword which Link wields.",
-            image: "images/fi zelda.jpg"
-        }
-    ];
-    res.json(characterList);
-});
-
 // New endpoint to get characters from CharacterList collection in MongoDB
 app.get('/api/CharacterList', async (req, res) => {
     try {
